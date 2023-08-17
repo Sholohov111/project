92be7cc0aa3c7305992eff16e2d23c670e19eb84
Sholohov111 <142454648+Sholohov111@users.noreply.github.com>
2023-08-17 13:43:39 +0300

    Add files via upload

---
+++ ДЗ переменные.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int money;
+            string name;
+            float health;
+            char buttonYes;
+            bool canPay;           
+            byte percentageOfProgress;
+            uint playersOnLine;
+            double distanceToThePoint;
+            sbyte height;
+            short width;
+
+
+
+
+
+
+
+        }
+    }
+}
